dabebe0c0fce3888470689df1575af7ecb9b5430
NikitaNezgovorov <Nezgovorov.nikita@gmail.com>
2020-12-07 20:36:38 +0300

    hw for lesson 1

---
+++ hw1_1.py
@@ -0,0 +1,26 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк
+# и сохраните в переменные, выведите на экран.
+hello = 'Привет!'
+num = 25
+print(hello)
+print(num)
+
+number = input('Введите число:\n>>> ')
+age = input('Введите возраст:\n>>> ')
+number = int(number)
+age = int(age)
+
+print(number)
+print(type(number))
+
+print(age)
+print(type(age))
+
+word = input('Введите слово:\n>>> ')
+name = input('Введите имя:\n>>> ')
+
+print(word)
+print(type(word))
+
+print(name)
+print(type(name))
